f88c551913d081a2d7e03c00ace8267ca2de83fe
Prado-SCamila <prado.camila.910423@gmail.com>
2020-11-19 13:13:27 -0300

    projeto finalizado

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Projeto_Senha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            string nome= Console.ReadLine();
+
+            Console.WriteLine("Agora digite a uma senha:");
+            string senha= Console.ReadLine();
+
+            
+            while(nome==senha){
+                Console.WriteLine("Senha inválida. Digite outra");
+                senha = Console.ReadLine();
+            }
+
+            
+            }
+        }
+    }
+
